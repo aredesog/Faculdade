9bacfec18234a5053fb3069d915a321ff6869e8f
Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2026-04-06 11:28:54 -0300

    projeto banco(1)

--- src/main/java/EXERCICIOS/Banco/main.java
+++ src/main/java/EXERCICIOS/Banco/main.java
@@ -1,10 +1,22 @@
 package EXERCICIOS.Banco;
 
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args) {
         Conta Pedro = new Conta(1000);
-        System.out.println(Pedro.getSaldo());
+        //EX1
+        System.out.println("Saldo inicial: " + Pedro.getSaldo());
         Pedro.depositar(500);
         System.out.println("Saldo do pedro: " + Pedro.getSaldo());
+
+        //EX2
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor para depositar: ");
+        double valorDeposito = scanner.nextDouble();
+        Pedro.depositar(valorDeposito);
+        System.out.println("Saldo do pedro: " + Pedro.getSaldo());
+
     }
 }
