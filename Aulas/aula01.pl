18090ea05d0b69c14e0e5fe1c67f18fd86938976
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2026-04-11 17:35:46 -0300

    adiciona aula sobre lógica e cláusulas em Prolog com exemplos de sintaxe

---
+++ Aulas/aula01.pl
@@ -0,0 +1,20 @@
+/* 
+Aula de revião de lógica e propiedades
+
+NEGAÇÃO
+CONJUNÇÃO (E)
+DISJUNÇÃO (OU)
+XOR (OU EXCLUSIVO)
+IGUALDADE (<->)
+IMPLICAÇÃO ( ->)
+
+Em Prolog funciona assim:
+    CONCLUSÃO <- IMPLICAÇÃO
+
+        EX:
+        não chove <- faz sol
+        falso <- chove e faz sol
+        não chove ou não faz sol <- verdadeiro
+        não afz sol <- chove
+*/
+
---
+++ Aulas/aula02.pl
@@ -0,0 +1,37 @@
+/*
+Aula sobre clausulas e programacao logica(sintaxe)
+*/ 
+
+/*
+m = 1 ; n = 0 (FATO) | A  <-
+m = 1 ; n > 0 (REGRA) | A <- b1,b2...bn
+m = 0 ; n > 0 (PERGUNTA) | <- b1,b2 ... bn
+m = 0 ; n = 0 (NULA) | <-
+
+Uso muito comum na regra po refutação!
+
+
+Na sintaxe Prolog:
+
+    CLAUSULAS
+
+    Conjunção (A , B)
+    Disjunção (A ; B)
+    Condicional (A :- B)
+
+    TERMOS
+
+
+*/
+
+/* fatos */
+pai(joao, joaquim).
+pai(joao, lucas).
+pai(pedro, silvia).
+pai(joaquim, bruno).
+mae(maria,joaquim).
+mae(maria,lucas).
+mae(laura, silvia).
+mae(silvia, bruno).
+/*regras*/
+esposa(X,Y) :- pai(Y,Z),mae(X,Z).
