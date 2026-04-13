97adc6d2c34266726face7b792a88aa64140c8e2
Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2026-04-13 09:43:19 -0300

    adiciona implementações de funções para análise, remoção de duplicados, troca e potência em listaEx1.pl

--- Exercicios/listaEx1.pl
+++ Exercicios/listaEx1.pl
@@ -1,2 +1,25 @@
-/*EX-1*/
-analisa_lista = 
+/*EX-1* - analisa/1 */
+analisa_lista([]) :- write('Lista vazia').
+analisa_lista([X|Y]) :- write('A cabeça é '), 
+    write(X), write(' Cauda: '), write(Y).
+
+/*EX-2 - remove/2 */
+remove_duplicados([],[]).
+
+remove_duplicados([X|Y], R) :- member(X, Y),
+    remove_duplicados(Y, R).
+
+remove_duplicados([X|Y], [X|R]) :- \+ member(X, Y),
+    remove_duplicados(Y, R).
+
+/*EX-3 - troca/4 */
+troca([],_,_,[]).
+troca([X|Y], A, B, [B|R]) :- X ==
+ A, troca(Y, A, B, R).
+troca([X|Y], A, B, [X|R]) :- X \= A,
+    troca(Y, A, B, R).
+
+/*EX-4 - potencia */
+potencia([], _).
+potencia([])
+
