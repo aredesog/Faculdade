460ba48642c61b613c365e6d39035790bb4b811c
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2026-04-13 21:07:31 -0300

    adiciona implementações das funções compara, penultimo e nono em p1-2025.pl

--- Exercicios/p1-2025.pl
+++ Exercicios/p1-2025.pl
@@ -1,2 +1,15 @@
-% QST-1
+/*compara*/
+compara([], []).
+compara([_|X], [_|Y]) :- 
+    compara(X, Y).
 
+/*penultimo*/
+penultimo ([A,_], A) :-!.
+penultimo ([_|B], C) :- penultimo (B, C).
+
+resolucao
+nono([a,b,c], [a,a|Y]).
+nono([b,c], [b,b|Y]).
+nono([c], [c,c|Y]).
+nono([],[]).
+nono([a,b,c],L)
