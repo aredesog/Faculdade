67b69f8ba992b99e4e71962b7aef5f0d48696e8a
Abner-Guimaraes <Abner-Guimaraes.sou.unifal-mg.edu.br>
2026-06-16 10:44:21 -0300

    adiciona implementação do predicado lsort e funções auxiliares para ordenação de listas

--- Trabalho/tp1.pl
+++ Trabalho/tp1.pl
@@ -1,3 +1,21 @@
 /*TRABALHO PROLOG - GUILHERME DE OLIVEIRA AREDES - 2025.1.08.026 */
 
-FIM
+/*Atv 1 - Ordenção de Listas de Listas*/
+
+% Predicado principal
+lsort(X, Y) :-
+    mapear_tamanhos(X, Z),
+    keysort(Z, W),
+    remover_chaves(W, Y).
+
+% Garante que cada sublista gere exatamente um par N-H
+mapear_tamanhos([], []).
+mapear_tamanhos([H | T], [N-H | R]) :-
+    length(H, N),
+    mapear_tamanhos(T, R).
+
+% Garante que apenas o elemento V seja extraído do par N-V
+remover_chaves([], []).
+remover_chaves([_N-V | T1], [V | T2]) :-
+    remover_chaves(T1, T2).
+
