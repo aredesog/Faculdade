1ea77a072fc21ec6dc926b41f4d2e3f8d5f5270b
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2026-06-22 15:10:45 -0300

    atualiza o tp1 de prolog com a implementacao do bubble sort

--- Trabalho/tp1.pl
+++ Trabalho/tp1.pl
@@ -1,50 +1,31 @@
-/*TRABALHO PROLOG - GUILHERME DE OLIVEIRA AREDES - 2025.1.08.026 */
+/*TRABALHO PROLOG NA UNHA*/
 
-/*Atv 1- Parte A: Ordenção de Listas de Listas*/
+'ATIVIDADE 1 - A:'
 
-% Predicado principal
-lsort(X, Y) :-
-    mapear_tamanhos(X, Z),
-    keysort(Z, W),
-    remover_chaves(W, Y).
+lsort(ListaEntrada, ListaOrdenada) :-
+    mapear_tamanhos(ListaEntrada, ListaComTamanhos),
+    bubble_sort(ListaComTamanhos, ListaOrdenadaComTamanhos),
+    remover_tamanhos(ListaOrdenadaComTamanhos, ListaOrdenada).
 
-% Garante que cada sublista gere exatamente um par N-H
+% Associa cada sublista ao seu tamanho
 mapear_tamanhos([], []).
-mapear_tamanhos([H | T], [N-H | R]) :-
-    length(H, N),
-    mapear_tamanhos(T, R).
+mapear_tamanhos([Sublista | Resto], [[Tamanho, Sublista] | RestoComTamanhos]) :-
+    length(Sublista, Tamanho),
+    mapear_tamanhos(Resto, RestoComTamanhos).
 
-% Garante que apenas o elemento V seja extraído do par N-V
-remover_chaves([], []).
-remover_chaves([_N-V | T1], [V | T2]) :-
-    remover_chaves(T1, T2).
+% Bubble Sort
+bubble_sort(Lista, Ordenada) :-
+    passagem(Lista, Lista1), !,
+    bubble_sort(Lista1, Ordenada).
+bubble_sort(Ordenada, Ordenada).
 
-/* Atv 1 - Parte B: Ordenação pela frequência dos comprimentos */
+% Uma passagem - troca pares fora de ordem
+passagem([[T1,S1],[T2,S2]|Resto], [[T2,S2],[T1,S1]|Resto]) :-
+    T1 > T2.
+passagem([Par|Resto], [Par|RestoPassagem]) :-
+    passagem(Resto, RestoPassagem).
 
-lfsort(X, Y) :-
-    mapear_tamanhos(X, Z),            % Reutiliza seu predicado: gera [Tam-Sublista]
-    extrair_tamanhos(Z, ListaTans),   % Extrai apenas os números dos tamanhos
-    mapear_frequencias(Z, ListaTans, W), % Transforma em [Freq-Sublista]
-    keysort(W, K),                    % Ordena pela frequência crescente
-    remover_chaves(K, Y).             % Reutiliza seu predicado: limpa e entrega as sublistas
-
-
-extrair_tamanhos([], []).
-extrair_tamanhos([Tam-_Sub | T1], [Tam | T2]) :-
-    extrair_tamanhos(T1, T2).
-
-
-contar_ocorrido(_, [], 0).
-contar_ocorrido(X, [H|T], Qtd) :-
-    X =:= H,
-    contar_ocorrido(X, T, QtdResto),
-    Qtd is QtdResto + 1.
-contar_ocorrido(X, [H|T], Qtd) :-
-    X =\= H,
-    contar_ocorrido(X, T, Qtd).
-
-mapear_frequencias([], _, []).
-mapear_frequencias([Tam-Sub | T1], ListaTans, [Freq-Sub | T2]) :-
-    contar_ocorrido(Tam, ListaTans, Freq),
-    mapear_frequencias(T1, ListaTans, T2).
-     
+% Remove os tamanhos, ficando com as sublistas
+remover_tamanhos([], []).
+remover_tamanhos([[_Tamanho, Sublista] | Resto], [Sublista | RestoSemTamanhos]) :-
+    remover_tamanhos(Resto, RestoSemTamanhos).
