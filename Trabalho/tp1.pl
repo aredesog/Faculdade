50076b04809b019a633d47171036d482600c8581
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2026-06-24 13:18:42 -0300

    trabalho feito!

--- Trabalho/tp1.pl
+++ Trabalho/tp1.pl
@@ -1,13 +1,12 @@
 /*TRABALHO PROLOG NA UNHA - GUILHERME DE OLIVEIRA AREDES - 2025.1.08.026*/
 
-'ATIVIDADE 1 - A:'
+% ATIVIDADE 1 - A:
 
 lsort(ListaEntrada, ListaOrdenada) :-
     mapear_tamanhos(ListaEntrada, ListaComTamanhos),
     bubble_sort(ListaComTamanhos, ListaOrdenadaComTamanhos),
     remover_tamanhos(ListaOrdenadaComTamanhos, ListaOrdenada).
 
-% Associa cada sublista ao seu tamanho
 mapear_tamanhos([], []).
 mapear_tamanhos([Sublista | Resto], [[Tamanho, Sublista] | RestoComTamanhos]) :-
     length(Sublista, Tamanho),
@@ -19,19 +18,16 @@ bubble_sort(Lista, Ordenada) :-
     bubble_sort(Lista1, Ordenada).
 bubble_sort(Ordenada, Ordenada).
 
-% Uma passagem, troca pares fora de ordem
 passagem([[T1,S1],[T2,S2]|Resto], [[T2,S2],[T1,S1]|Resto]) :-
     T1 > T2.
 passagem([Par|Resto], [Par|RestoPassagem]) :-
     passagem(Resto, RestoPassagem).
 
-% Remove os tamanhos, ficando com as sublistas
 remover_tamanhos([], []).
 remover_tamanhos([[Tamanho, Sublista] | Resto], [Sublista | RestoSemTamanhos]) :-
     remover_tamanhos(Resto, RestoSemTamanhos).
 
-
-'ATIVIDADE 2 - B: '
+% ATIVIDADE 2- B 
 
 lfsort(ListaEntrada, ListaOrdenada) :-
     mapear_tamanhos(ListaEntrada, ListaComTamanhos),
@@ -39,7 +35,6 @@ lfsort(ListaEntrada, ListaOrdenada) :-
     bubblesort_freq(ListaComFrequencias, ListaOrdenadaComFreq),
     remover_extras(ListaOrdenadaComFreq, ListaOrdenada).
 
-% Conta quantas vezes um tamanho aparece na lista
 contar_frequencia(_, [], 0).
 contar_frequencia(Tamanho, [[Tamanho, _] | Resto], Frequencia) :-
     contar_frequencia(Tamanho, Resto, FrequenciaResto),
@@ -48,15 +43,17 @@ contar_frequencia(Tamanho, [[OutroTamanho, _] | Resto], Frequencia) :-
     Tamanho \= OutroTamanho,
     contar_frequencia(Tamanho, Resto, Frequencia).
 
-% Pega cada par [Tamanho, Sublista] a sua frequencia
-calcular_frequencias([], []).
-calcular_frequencias([[Tamanho, Sublista] | Resto], [[Frequencia, Tamanho, Sublista] | RestoComFreq]) :-
-    contar_frequencia(Tamanho, [[Tamanho, Sublista] | Resto], Frequencia),
-    calcular_frequencias(Resto, RestoComFreq).
+calcular_frequencias(Lista, ListaComFreq) :-
+    calcular_frequencias(Lista, Lista, ListaComFreq).
 
-% Bubble Sort 
+calcular_frequencias([], _, []).
+calcular_frequencias([[Tamanho, Sublista] | Resto], ListaToda, [[Frequencia, Tamanho, Sublista] | RestoComFreq]) :-
+    contar_frequencia(Tamanho, ListaToda, Frequencia),
+    calcular_frequencias(Resto, ListaToda, RestoComFreq).
+
 bubblesort_freq(Lista, Ordenada) :-
-    passagem_freq(Lista, Lista1), !,
+    passagem_freq(Lista, Lista1),
+    Lista \= Lista1,
     bubblesort_freq(Lista1, Ordenada).
 bubblesort_freq(Ordenada, Ordenada).
 
@@ -65,10 +62,8 @@ passagem_freq([[F1,T1,S1],[F2,T2,S2]|Resto], [[F2,T2,S2],[F1,T1,S1]|Resto]) :-
 passagem_freq([Par|Resto], [Par|RestoPassagem]) :-
     passagem_freq(Resto, RestoPassagem).
 
-% Remove frequencia e tamanho, ficando so com as sublistas
 remover_extras([], []).
-remover_extras([[Freq, _Tamanho, Sublista] | Resto], [Sublista | RestoSemExtras]) :-
+remover_extras([[_Freq, _Tamanho, Sublista] | Resto], [Sublista | RestoSemExtras]) :-
     remover_extras(Resto, RestoSemExtras).
-
 
 
