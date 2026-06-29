144765366dd05563a58d33323bbdc1596528e008
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2026-06-29 01:15:00 -0300

    adiciona possiveis perguntas e comentario no calcular_freq que tinha esquecido

--- Trabalho/tp1.pl
+++ Trabalho/tp1.pl
@@ -44,7 +44,11 @@ contar_frequencia(Tamanho, [[OutroTamanho, _] | Resto], Frequencia) :-
     contar_frequencia(Tamanho, Resto, Frequencia).
 
 calcular_frequencias(Lista, ListaComFreq) :-
-    calcular_frequencias(Lista, Lista, ListaComFreq).
+    calcular_frequencias(Lista, Lista, ListaComFreq). 
+'''
+Na primeira lisat ele vai removendo os elementos ja processados, na segunda lista ele contabiliza
+da lista original quantas vezes aparece/freq
+'''
 
 calcular_frequencias([], _, []).
 calcular_frequencias([[Tamanho, Sublista] | Resto], ListaToda, [[Frequencia, Tamanho, Sublista] | RestoComFreq]) :-
@@ -66,4 +70,17 @@ remover_extras([], []).
 remover_extras([[_Freq, _Tamanho, Sublista] | Resto], [Sublista | RestoSemExtras]) :-
     remover_extras(Resto, RestoSemExtras).
 
+
+'''
+Ótimas perguntas que eu faria:
+
+"Por que você precisa passar a Lista duas vezes no calcular_frequencias? Não daria pra fazer com um só parâmetro?"
+"Qual é a diferença entre o primeiro e o terceiro caso do contar_frequencia? Por que os dois são necessários?"
+"Por que no bubblesort_freq você usa Lista \= Lista1 e no bubble_sort você não usa?"
+"Como funciona a recursão no mapear_tamanhos? O que acontece em cada nível?"
+"Se você remover o underscore _ das variáveis que não usa, o código deixa de funcionar ou só gera aviso?"
+"Explica passo a passo o que acontece quando você chama lfsort([[a,b,c],[d,e],[o]], L)."
+
+Essas seriam as principais!
+'''
 
